a6bafb039b68c8c01177c788fdaaeeaef0a3b723
sllopezv <sllopezv@unal.edu.co>
2024-02-19 17:48:58 -0500

    adds asiento, auto and motor

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,36 @@
+package test;
+
+public class Auto {
+	int registro;
+	int precio;
+	String modelo;
+	String marca;
+	Motor motor;
+	Asiento [] asientos;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int numeroAsientos = 0;
+		for (int i = 0 ; i < asientos.length ; i++) {
+
+			if(asientos[i] != null) {
+				numeroAsientos++;
+			}
+		}
+		return numeroAsientos;
+	}
+	
+	String verificarIntegridad() {
+		if(this.registro == this.motor.registro) {
+			for(int i = 0 ; i < asientos.length ; i++) {
+				if(asientos[i] != null && asientos[i].registro != registro) {
+					return "Piezas no son originales.";
+				}
+			}
+			return "Auto original.";
+		}
+		else {
+			return "Piezas no son originales.";
+		}
+	}
+}
